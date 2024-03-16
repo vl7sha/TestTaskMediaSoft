8a9c7b4830c242f3acd542f44c96ff9edf136eeb
vl7sha <vl7shab@yandex.ru>
2024-03-16 22:06:20 +0400

    init project

---
+++ src/main/java/ru/vl7sha/testmediasoftacademia/TestMediaSoftAcademiaApplication.java
@@ -0,0 +1,13 @@
+package ru.vl7sha.testmediasoftacademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMediaSoftAcademiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMediaSoftAcademiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vl7sha/testmediasoftacademia/TestMediaSoftAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vl7sha.testmediasoftacademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMediaSoftAcademiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
